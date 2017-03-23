8c60486accd55d6bbd4aef5d54ddfb3aa5cef1c0
Andres <andresnrx@gmail.com>
2017-03-22 22:43:12 -0700

    split player onto its own mod

--- src/duck_husky_wedding/mod.rs
+++ src/duck_husky_wedding/mod.rs
@@ -1,11 +1,12 @@
 pub mod game_data;
+mod player;
 
 use errors::*;
+use self::player::Player;
 use self::game_data::GameData;
 
-use glm;
 use moho::input_manager::InputManager;
-use moho::resource_manager::{Renderer, ResourceLoader, TileSheet, FrameAnimator};
+use moho::resource_manager::Renderer;
 use moho::MohoEngine;
 use moho::timer::Timer;
 
@@ -14,10 +15,7 @@ use std::time::Duration;
 pub struct DuckHuskyWedding<E: MohoEngine> {
     input_manager: InputManager<E::EventPump>,
     renderer: E::Renderer,
-    sheet: TileSheet,
-    animator: FrameAnimator,
-    dimensions: glm::UVec2,
-    position: glm::IVec2,
+    player: Player,
 }
 
 impl<E: MohoEngine> DuckHuskyWedding<E> {
@@ -25,19 +23,11 @@ impl<E: MohoEngine> DuckHuskyWedding<E> {
                input_manager: InputManager<E::EventPump>,
                game_data: GameData)
                -> Self {
-        let duck_data = game_data.duck;
-        let duck_file = format!("media/sprites/{}", duck_data.file_name);
-        let duck_texture = renderer.load_texture(&duck_file).unwrap();
-        let sheet = TileSheet::new(duck_data.tiles.into(), duck_texture);
-        let animator = FrameAnimator::new(duck_data.frames, Duration::from_millis(60), true);
-        println!("{:?}", duck_texture.id);
+        let player = Player::load(game_data.duck, &renderer).unwrap();
         DuckHuskyWedding {
             input_manager: input_manager,
-            sheet: sheet,
-            animator: animator,
             renderer: renderer,
-            dimensions: duck_data.out_size.into(),
-            position: glm::ivec2(0, 300),
+            player: player,
         }
     }
 
@@ -47,7 +37,6 @@ impl<E: MohoEngine> DuckHuskyWedding<E> {
         let update_duration = Duration::new(0, 1000000000 / GAME_SPEED);
         let mut timer = Timer::new();
         let mut delta: Duration = Default::default();
-        self.animator.start();
         while !self.game_quit() {
             let game_time = timer.update();
             delta += game_time.since_update;
@@ -64,7 +53,7 @@ impl<E: MohoEngine> DuckHuskyWedding<E> {
             if self.game_quit() {
                 break;
             }
-            self.animator.animate(delta);
+            self.player.animate(delta);
             let interpolation = delta.subsec_nanos() as f64 / update_duration.subsec_nanos() as f64;
             self.draw(interpolation)?;
         }
@@ -72,17 +61,12 @@ impl<E: MohoEngine> DuckHuskyWedding<E> {
     }
 
     fn update(&mut self) {
-        self.position.x = (self.position.x + 5) % 1280;
+        self.player.update();
     }
 
     fn draw(&mut self, interpolation: f64) -> Result<()> {
         self.renderer.clear();
-        let tile = self.sheet.tile(self.animator.frame().unwrap());
-        let dst_rect = glm::ivec4(self.position.x,
-                                  self.position.y,
-                                  self.dimensions.x as i32,
-                                  self.dimensions.y as i32);
-        self.renderer.render(&tile, dst_rect);
+        self.renderer.show(&self.player)?;
         self.renderer.present();
         Ok(())
     }
---
+++ src/duck_husky_wedding/player.rs
@@ -0,0 +1,53 @@
+use duck_husky_wedding::game_data::SpriteData;
+use errors::*;
+
+use glm;
+use moho::errors as moho_errors;
+use moho::resource_manager::{FrameAnimator, Renderer, ResourceLoader, Scene, TileSheet};
+
+use std::time::Duration;
+
+pub struct Player {
+    sheet: TileSheet,
+    animator: FrameAnimator,
+    dimensions: glm::UVec2,
+    position: glm::IVec2,
+}
+
+impl Player {
+    pub fn load<R: ResourceLoader>(data: SpriteData, loader: &R) -> Result<Self> {
+        let file_name = format!("media/sprites/{}", data.file_name);
+        let texture = loader.load_texture(&file_name)?;
+        let sheet = TileSheet::new(data.tiles.into(), texture);
+        let animator = FrameAnimator::new(data.frames, Duration::from_millis(50), true);
+        let player = Player {
+            sheet: sheet,
+            animator: animator,
+            dimensions: data.out_size.into(),
+            position: glm::ivec2(0, 300),
+        };
+        Ok(player)
+    }
+
+    pub fn animate(&mut self, delta: Duration) {
+        match self.animator.frame() {
+            None => self.animator.start(),
+            Some(_) => self.animator.animate(delta),
+        }
+    }
+
+    pub fn update(&mut self) {
+        self.position.x = (self.position.x + 5) % 1280;
+    }
+}
+
+impl Scene for Player {
+    fn show<R: Renderer>(&self, renderer: &mut R) -> moho_errors::Result<()> {
+        let tile = self.sheet.tile(self.animator.frame().unwrap());
+        let dst_rect = glm::ivec4(self.position.x,
+                                  self.position.y,
+                                  self.dimensions.x as i32,
+                                  self.dimensions.y as i32);
+        renderer.render(&tile, dst_rect)
+    }
+}
