267aebe5089b145bcb4062ab98f423d4447eada3
Andres <andresnrx@gmail.com>
2017-05-10 01:44:33 -0700

    player flips according to direction of movement

--- src/duck_husky_wedding/player.rs
+++ src/duck_husky_wedding/player.rs
@@ -4,7 +4,7 @@ use glm;
 use moho::animation::{Animation, AnimationData, AnimatorData, TileSheet};
 use moho::errors as moho_errors;
 use moho::input;
-use moho::renderer::{Renderer, Scene, Show, Texture};
+use moho::renderer::{Renderer, Scene, Show, Texture, TextureFlip};
 use moho::shape::Rectangle;
 use sdl2::keyboard::Keycode;
 
@@ -14,7 +14,7 @@ use std::rc::Rc;
 pub struct Player<T> {
     animation: Animation<T>,
     body: Rectangle,
-    velocity: f64,
+    backwards: bool,
 }
 
 impl<T> Player<T> {
@@ -31,26 +31,24 @@ impl<T> Player<T> {
         Player {
             animation: animation_data.start(),
             body: body,
-            velocity: 0.,
+            backwards: false,
         }
     }
 
     pub fn update(&mut self, delta: Duration, input: &input::State) {
         self.animation.animate(delta);
-        let mut velocity = 0.;
-        if input.is_key_down(Keycode::Left) {
-            velocity -= 5.;
+        let left = input.is_key_down(Keycode::Left);
+        let right = input.is_key_down(Keycode::Right);
+        if left ^ right {
+            let window = Rectangle {
+                top_left: glm::dvec2(0., 0.),
+                dims: glm::dvec2(1280., 720.),
+            };
+            self.backwards = left;
+            let velocity = if left { -5. } else { 5. };
+            self.body.top_left.x += velocity;
+            self.body = Self::clamp(&self.body, &window);
         }
-        if input.is_key_down(Keycode::Right) {
-            velocity += 5.;
-        }
-        self.velocity = velocity;
-        self.body.top_left.x += self.velocity;
-        let window = Rectangle {
-            top_left: glm::dvec2(0., 0.),
-            dims: glm::dvec2(1280., 720.),
-        };
-        self.body = Self::clamp(&self.body, &window);
     }
 
     fn clamp(shape: &Rectangle, window: &Rectangle) -> Rectangle {
@@ -78,9 +76,11 @@ impl<'t, T, R> Scene<R> for Player<T>
                                                 self.body.top_left.y,
                                                 self.body.dims.x,
                                                 self.body.dims.y));
-        renderer
-            .with_asset(&self.animation.tile())
-            .at(&dst_rect)
-            .copy()
+        let tile = self.animation.tile();
+        let mut partial = renderer.with_asset(&tile).at(&dst_rect);
+        if self.backwards {
+            partial = partial.flip(TextureFlip::Horizontal)
+        }
+        partial.copy()
     }
 }
