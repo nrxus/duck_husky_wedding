72382a55a016bce9d617c7d771066d467322f475
Pivotal <pivotal@example.com>
2017-07-03 10:50:55 -0700

    display correct block texture

--- src/duck_husky_wedding/obstacle.rs
+++ src/duck_husky_wedding/obstacle.rs
@@ -22,16 +22,40 @@ impl<T> Vec2D<T> {
     }
 }
 
+struct Textures<T> {
+    center: Rc<T>,
+    left: Rc<T>,
+    right: Rc<T>,
+    top_center: Rc<T>,
+    top_left: Rc<T>,
+    top_right: Rc<T>,
+}
+
+impl<T> Clone for Textures<T> {
+    fn clone(&self) -> Self {
+        Textures {
+            center: self.center.clone(),
+            left: self.left.clone(),
+            right: self.right.clone(),
+            top_center: self.top_center.clone(),
+            top_left: self.top_left.clone(),
+            top_right: self.top_right.clone(),
+        }
+    }
+}
+
 pub struct Obstacle<T> {
     tiles: Vec2D<Rc<T>>,
     dims: glm::UVec2,
     tl: glm::IVec2,
+    textures: Textures<T>,
 }
 
 impl<T> Clone for Obstacle<T> {
     fn clone(&self) -> Self {
         Obstacle {
             tiles: self.tiles.clone(),
+            textures: self.textures.clone(),
             dims: self.dims,
             tl: self.tl,
         }
@@ -54,7 +78,20 @@ impl<T> Obstacle<T> {
         let dims = ground.out_size.into();
         let tl: glm::UVec2 = obstacle.top_left.into();
         let tl = glm::to_ivec2(tl * dims);
-        Ok(Obstacle { tiles, dims, tl })
+        let textures = Textures {
+            center: ground.center.load(texture_manager)?,
+            left: ground.left.load(texture_manager)?,
+            right: ground.right.load(texture_manager)?,
+            top_center: ground.top.load(texture_manager)?,
+            top_left: ground.top_left.load(texture_manager)?,
+            top_right: ground.top_right.load(texture_manager)?,
+        };
+        Ok(Obstacle {
+            tiles,
+            dims,
+            tl,
+            textures,
+        })
     }
 
     pub fn mtv(&self, object: &Rectangle) -> Option<glm::DVec2> {
@@ -71,8 +108,25 @@ impl<'t, R: Renderer<'t>> Scene<R> for Obstacle<R::Texture> {
         (0..self.tiles.count.x)
             .flat_map(|i| (0..self.tiles.count.y).map(move |j| (i, j)))
             .map(|(i, j)| {
+                let texture = if j == 0 {
+                    if i == self.tiles.count.x - 1 {
+                        &self.textures.top_right
+                    } else if i == 0 {
+                        &self.textures.top_left
+                    } else {
+                        &self.textures.top_center
+                    }
+                } else {
+                     if i == self.tiles.count.x - 1 {
+                        &self.textures.right
+                    } else if i == 0 {
+                        &self.textures.left
+                    } else {
+                        &self.textures.center
+                    }
+                };
                 (
-                    self.tiles.get((i, j)),
+                    texture,
                     glm::ivec4(
                         self.tl.x + (self.dims.x * i) as i32,
                         self.tl.y + (self.dims.y * j) as i32,
