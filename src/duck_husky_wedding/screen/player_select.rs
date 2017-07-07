59b54c07ba87c87f29e127d430f18bd716358ddf
Pivotal <pivotal@example.com>
2017-07-07 14:05:51 -0700

    added collect + avoid text on player select screen

--- src/duck_husky_wedding/screen/player_select.rs
+++ src/duck_husky_wedding/screen/player_select.rs
@@ -14,12 +14,16 @@ use std::time::Duration;
 
 pub struct PlayerSelect<T> {
     title: Rc<T>,
+    collect_text: Rc<T>,
+    avoid_text: Rc<T>,
     husky: button::Animated<T>,
     duck: button::Animated<T>,
 }
 
 pub struct Data<T> {
     title: Rc<T>,
+    collect_text: Rc<T>,
+    avoid_text: Rc<T>,
     husky: button::Animated<T>,
     duck: button::Animated<T>,
 }
@@ -44,28 +48,38 @@ impl<T> Data<T> {
         let font = font_manager.load(&font_details)?;
         let title_color = ColorRGBA(255, 255, 0, 255);
         let title = Rc::new(texturizer.texturize(&*font, "Select Player", &title_color)?);
+        let button_distance = 50.;
+        
         let husky = {
             let data = &data.husky;
             let idle = data.idle_texture.load(texture_manager)?;
             let animation = data.animation.load(texture_manager)?;
             let dims: glm::DVec2 = data.out_size.into();
             let dims = dims * 1.5;
-            let top_left = glm::dvec2(300., 250. - dims.y);
+            let top_left = glm::dvec2(640. - dims.x - button_distance, 250. - dims.y);
             let body = Rectangle { top_left, dims };
             button::Animated::new(idle, animation, body)
         };
-         let duck = {
+        let duck = {
             let data = &data.duck;
             let idle = data.idle_texture.load(texture_manager)?;
             let animation = data.animation.load(texture_manager)?;
             let dims: glm::DVec2 = data.out_size.into();
             let dims = dims * 1.5;
-            let top_left = glm::dvec2(750., 250. - dims.y);
+            let top_left = glm::dvec2(640. + button_distance, 250. - dims.y);
             let body = Rectangle { top_left, dims };
             button::Animated::new(idle, animation, body)
         };
+        let collect_text = Rc::new(texturizer.texturize(&*font, "Collect", &title_color)?);
+        let avoid_text = Rc::new(texturizer.texturize(&*font, "Avoid", &title_color)?);
 
-        Ok(Data { title, husky, duck })
+        Ok(Data {
+            title,
+            husky,
+            duck,
+            collect_text,
+            avoid_text,
+        })
     }
 
     pub fn activate(&self) -> PlayerSelect<T> {
@@ -73,6 +87,8 @@ impl<T> Data<T> {
             title: self.title.clone(),
             duck: self.duck.clone(),
             husky: self.husky.clone(),
+            collect_text: self.collect_text.clone(),
+            avoid_text: self.avoid_text.clone(),
         }
     }
 }
@@ -96,10 +112,23 @@ where
     R::Texture: Texture,
 {
     fn show(&self, renderer: &mut R) -> moho_errors::Result<()> {
-        let title_dims = glm::to_ivec2(self.title.dims());
-        let title_rectangle = glm::ivec4(640 - title_dims.x / 2, 0, title_dims.x, title_dims.y);
+        let title_rectangle = {
+            let dims = glm::to_ivec2(self.title.dims());
+            glm::ivec4(640 - dims.x / 2, 0, dims.x, dims.y)
+        };
+        let collect_rectangle = {
+            let dims = glm::to_ivec2(self.collect_text.dims());
+            glm::ivec4(320 - dims.x / 2, 350, dims.x, dims.y)
+        };
+        let avoid_rectangle = {
+            let dims = glm::to_ivec2(self.avoid_text.dims());
+            glm::ivec4(960 - dims.x / 2, 350, dims.x, dims.y)
+        };
+
         renderer.show(&self.husky)?;
         renderer.show(&self.duck)?;
-        renderer.copy(&self.title, options::at(&title_rectangle))
+        renderer.copy(&self.title, options::at(&title_rectangle))?;
+        renderer.copy(&self.collect_text, options::at(&collect_rectangle))?;
+        renderer.copy(&self.avoid_text, options::at(&avoid_rectangle))
     }
 }
