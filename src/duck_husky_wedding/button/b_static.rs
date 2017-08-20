3b5173000f646025dbcd7b665854458bdc1a4cde
Andres <andresnrx@gmail.com>
2017-08-19 17:08:41 -0700

    use keyboard instead of the mouse to control buttons in main menu

--- src/duck_husky_wedding/button/b_static.rs
+++ src/duck_husky_wedding/button/b_static.rs
@@ -1,9 +1,7 @@
-use super::Button;
 use errors::*;
 
 use glm;
 use moho::errors as moho_errors;
-use moho::shape::Rectangle;
 use moho::renderer::{options, ColorRGBA, Font, FontTexturizer, Renderer, Scene};
 
 use std::rc::Rc;
@@ -11,8 +9,8 @@ use std::rc::Rc;
 pub struct Static<T> {
     idle: Rc<T>,
     hover: Rc<T>,
-    is_hovering: bool,
-    pub body: Rectangle,
+    dst: glm::IVec4,
+    pub is_selected: bool,
 }
 
 impl<T> Clone for Static<T> {
@@ -20,19 +18,9 @@ impl<T> Clone for Static<T> {
         Static {
             idle: self.idle.clone(),
             hover: self.hover.clone(),
-            body: self.body.clone(),
-            is_hovering: false,
+            dst: self.dst,
+            is_selected: false,
         }
-    }
-}
-
-impl<T> Button for Static<T> {
-    fn body(&self) -> &Rectangle {
-        &self.body
-    }
-
-    fn on_hover(&mut self, hovers: bool) {
-        self.is_hovering = hovers
     }
 }
 
@@ -47,44 +35,22 @@ impl<T> Static<T> {
         F: Font,
         FT: FontTexturizer<'t, F, Texture = T>,
     {
-        let dims = glm::to_dvec2(font.measure(text)?);
-        let body = Rectangle {
-            top_left: glm::dvec2(center.x as f64 - dims.x / 2., center.y as f64 - dims.y / 2.),
-            dims: dims,
-        };
+        let dims = glm::to_ivec2(font.measure(text)?);
+        let dst = glm::ivec4(center.x - dims.x / 2, center.y - dims.y / 2, dims.x, dims.y);
 
-        Self::text_at(text, texturizer, font, body)
-    }
-
-    pub fn from_text<'t, F, FT>(
-        text: &str,
-        texturizer: &'t FT,
-        font: &F,
-        tl: glm::IVec2,
-    ) -> Result<Self>
-    where
-        F: Font,
-        FT: FontTexturizer<'t, F, Texture = T>,
-    {
-        let dims = font.measure(text)?;
-        let body = Rectangle {
-            top_left: glm::to_dvec2(tl),
-            dims: glm::to_dvec2(dims),
-        };
-
-        Self::text_at(text, texturizer, font, body)
+        Self::text_at(text, texturizer, font, dst)
     }
 
     pub fn text_at<'t, F, FT>(
         text: &str,
         texturizer: &'t FT,
         font: &F,
-        body: Rectangle,
+        dst: glm::IVec4,
     ) -> Result<Self>
     where
         FT: FontTexturizer<'t, F, Texture = T>,
     {
-        let is_hovering = false;
+        let is_selected = false;
         let idle = Rc::new(texturizer
             .texturize(font, text, &ColorRGBA(255, 255, 255, 255))?);
         let hover = Rc::new(texturizer
@@ -92,27 +58,20 @@ impl<T> Static<T> {
         Ok(Static {
             idle,
             hover,
-            is_hovering,
-            body,
+            is_selected,
+            dst,
         })
     }
 }
 
 impl<'t, R: Renderer<'t>> Scene<R> for Static<R::Texture> {
     fn show(&self, renderer: &mut R) -> moho_errors::Result<()> {
-        let dst_rect = glm::to_ivec4(glm::dvec4(
-            self.body.top_left.x,
-            self.body.top_left.y,
-            self.body.dims.x,
-            self.body.dims.y,
-        ));
-
-        let texture = if self.is_hovering {
+        let texture = if self.is_selected {
             &*self.hover
         } else {
             &*self.idle
         };
 
-        renderer.copy(texture, options::at(&dst_rect))
+        renderer.copy(texture, options::at(&self.dst))
     }
 }
--- src/duck_husky_wedding/screen/menu.rs
+++ src/duck_husky_wedding/screen/menu.rs
@@ -1,5 +1,5 @@
 use errors::*;
-use duck_husky_wedding::button::{self, Button};
+use duck_husky_wedding::button;
 
 use glm;
 use moho::errors as moho_errors;
@@ -8,17 +8,16 @@ use moho::renderer::{options, ColorRGBA, Font, FontDetails, FontLoader, FontMana
                      FontTexturizer, Renderer, Scene, Texture};
 
 use std::rc::Rc;
+use sdl2::keyboard::Keycode;
 
 pub struct Menu<T> {
     title: Rc<T>,
-    new_game: button::Static<T>,
-    high_score: button::Static<T>,
+    button_manager: ButtonManager<T>,
 }
 
 pub struct Data<T> {
     title: Rc<T>,
-    new_game: button::Static<T>,
-    high_score: button::Static<T>,
+    button_manager: ButtonManager<T>,
 }
 
 impl<T> Data<T> {
@@ -39,39 +38,30 @@ impl<T> Data<T> {
         let title_color = ColorRGBA(255, 255, 0, 255);
         let title = Rc::new(texturizer
             .texturize(&*font, "Husky Loves Ducky", &title_color)?);
-        let new_game =
-            button::Static::center_text("New Game", texturizer, &*font, glm::ivec2(640, 250))?;
-        let high_score =
-            button::Static::center_text("High Scores", texturizer, &*font, glm::ivec2(640, 450))?;
+        let button_manager = ButtonManager::load(texturizer, &*font)?;
         Ok(Data {
             title,
-            new_game,
-            high_score,
+            button_manager,
         })
     }
 
     pub fn activate(&self) -> Menu<T> {
         let title = self.title.clone();
-        let new_game = self.new_game.clone();
-        let high_score = self.high_score.clone();
+        let button_manager = self.button_manager.clone();
 
         Menu {
             title,
-            new_game,
-            high_score,
+            button_manager,
         }
     }
 }
 
 impl<T> Menu<T> {
     pub fn update(&mut self, input: &input::State) -> Option<super::Kind> {
-        if self.new_game.update(input) {
-            Some(super::Kind::PlayerSelect)
-        } else if self.high_score.update(input) {
-            Some(super::Kind::HighScore)
-        } else {
-            None
-        }
+        self.button_manager.update(input).map(|b| match b {
+            ButtonKind::HighScore => super::Kind::HighScore,
+            ButtonKind::NewGame => super::Kind::PlayerSelect,
+        })
     }
 }
 
@@ -80,10 +70,87 @@ where
     R::Texture: Texture,
 {
     fn show(&self, renderer: &mut R) -> moho_errors::Result<()> {
+        renderer.show(&self.button_manager)?;
         let title_dims = glm::to_ivec2(self.title.dims());
         let title_rectangle = glm::ivec4(640 - title_dims.x / 2, 0, title_dims.x, title_dims.y);
-        renderer.show(&self.new_game)?;
-        renderer.show(&self.high_score)?;
         renderer.copy(&self.title, options::at(&title_rectangle))
+    }
+}
+
+#[derive(Clone, Copy)]
+enum ButtonKind {
+    NewGame,
+    HighScore,
+}
+
+struct ButtonManager<T> {
+    selected: ButtonKind,
+    new_game: button::Static<T>,
+    high_score: button::Static<T>,
+}
+
+impl<T> Clone for ButtonManager<T> {
+    fn clone(&self) -> Self {
+        let mut new_game = self.new_game.clone();
+        new_game.is_selected = true;
+
+        ButtonManager {
+            new_game: new_game,
+            selected: self.selected,
+            high_score: self.high_score.clone(),
+        }
+    }
+}
+
+impl<T> ButtonManager<T> {
+    pub fn load<'t, FT, F>(texturizer: &'t FT, font: &F) -> Result<Self>
+    where
+        F: Font,
+        FT: FontTexturizer<'t, F, Texture = T>,
+    {
+        let mut new_game =
+            button::Static::center_text("New Game", texturizer, font, glm::ivec2(640, 250))?;
+        new_game.is_selected = true;
+
+        let high_score =
+            button::Static::center_text("High Scores", texturizer, font, glm::ivec2(640, 450))?;
+        Ok(ButtonManager {
+            new_game,
+            high_score,
+            selected: ButtonKind::NewGame,
+        })
+    }
+
+    pub fn update(&mut self, input: &input::State) -> Option<ButtonKind> {
+        if input.did_press_key(Keycode::Down) ^ input.did_press_key(Keycode::Up) {
+            self.selected = match self.selected {
+                ButtonKind::NewGame => {
+                    self.new_game.is_selected = false;
+                    self.high_score.is_selected = true;
+                    ButtonKind::HighScore
+                }
+                ButtonKind::HighScore => {
+                    self.high_score.is_selected = false;
+                    self.new_game.is_selected = true;
+                    ButtonKind::NewGame
+                }
+            }
+        }
+
+        if input.did_press_key(Keycode::Return) {
+            Some(self.selected)
+        } else {
+            None
+        }
+    }
+}
+
+impl<'t, R: Renderer<'t>> Scene<R> for ButtonManager<R::Texture>
+where
+    R::Texture: Texture,
+{
+    fn show(&self, renderer: &mut R) -> moho_errors::Result<()> {
+        renderer.show(&self.new_game)?;
+        renderer.show(&self.high_score)
     }
 }
